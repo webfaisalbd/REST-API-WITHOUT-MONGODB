4f128f272840854590a628ddd3854bf4391f8c60
webfaisalbd <web.faisal.bd@gmail.com>
2022-07-30 11:33:29 +0600

    create more data

--- models/users.model.js
+++ models/users.model.js
@@ -3,13 +3,48 @@ const { v4: uuidv4 } = require("uuid");
 const users = [
     {
         id: uuidv4(),
-        username: "faisal",
+        username: "Faisal",
         email: "faisal@gmail.com"
     },
     {
         id: uuidv4(),
-        username: "tamim",
-        email: "tamim.@gmail.com"
+        username: "Farhana",
+        email: "farhana@gmail.com"
+    },
+    {
+        id: uuidv4(),
+        username: "Ahmed",
+        email: "ahmed@gmail.com"
+    },
+    {
+        id: uuidv4(),
+        username: "Shajib",
+        email: "shajib@gmail.com"
+    },
+    {
+        id: uuidv4(),
+        username: "Hasan",
+        email: "hasan@gmail.com"
+    },
+    {
+        id: uuidv4(),
+        username: "Tasmi",
+        email: "tasmi@gmail.com"
+    },
+    {
+        id: uuidv4(),
+        username: "Tasnim",
+        email: "tasnim@gmail.com"
+    },
+    {
+        id: uuidv4(),
+        username: "Sruti",
+        email: "sruti@gmail.com"
+    },
+    {
+        id: uuidv4(),
+        username: "Iqra",
+        email: "iqra@gmail.com"
     }
 ];
 
