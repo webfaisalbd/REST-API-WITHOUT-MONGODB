9644d996c1597441cf4bbb7bdbeb85a4be5b5dc0
webfaisalbd <web.faisal.bd@gmail.com>
2023-09-11 12:20:22 +0600

    reset

--- index.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-require("dotenv").config();
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, ()=> {
-    console.log(`running server at http://localhost:${PORT}`);
-})
